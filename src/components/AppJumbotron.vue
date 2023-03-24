62d17ed386d463f36dc0074256a4f89275c08aff
Daniele Martini <ing.martini@gmail.com>
2023-03-24 17:13:16 +0100

    finito

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+    <div class="prova">
+        <img src="/vite.svg" />
+    </div>
+</template>
+
+<style scoped>
+.prova {
+    background: red;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1 class="prova">Titolo della pagina</h1>
+</template>
+
+<style scoped>
+.prova {
+    color: green;
+}
+</style>
